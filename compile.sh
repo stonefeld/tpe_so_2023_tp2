178423b64871564a4041656c05613aae7a9efe51
stonefeld <theo.stanfield@gmail.com>
2023-05-22 18:47:04 -0300

    docker-start.sh y docker-run.sh combinados en uno con flags para correr y debuguear

---
+++ compile.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+print_error() {
+    echo "$1"
+    exit 1
+}
+
+command -v docker >/dev/null 2>&1 || print_error "please install docker first"
+container_name="tpe_arqui_2023"
+
+DEBUG=false
+RUN=false
+
+for i in $@; do
+    case "$i" in
+        "-d") DEBUG=true;;
+        "-r") RUN=true;;
+    esac
+done
+
+[ "$DEBUG" = true ] && makecmd="clean debug" || makecmd="clean all"
+
+# if container doesn't exist create it
+if ! docker ps -a | grep "$container_name" >/dev/null 2>&1; then
+    docker run -d -v ${PWD}:/sources/ --security-opt=seccomp:unconfined \
+        -e 'GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01"' \
+        --name "$container_name" -it agodio/itba-so:1.0
+    docker exec "$container_name" \
+        /bin/bash -c "groupadd -g $(id -g) -o user && useradd -m -u $(id -u) -g $(id -g) -o -s /bin/bash user"
+fi
+
+# start the container
+docker ps | grep "$container_name" >/dev/null 2>&1 || docker start "$container_name"
+
+# compile the code and run
+docker exec -u user -t "$container_name" \
+    /bin/bash -lc "cd /sources/Toolchain && make && cd .. && make $makecmd"
+[ "$?" -eq 0 ] && [ "$RUN" = true ] && ./run.sh
+exit 0

ff648a20c0381fa6b1663e5c66875f703df89442
Theo Stanfield <theo.stanfield@gmail.com>
2023-10-09 17:35:23 -0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -25,7 +25,7 @@ done
 if ! docker ps -a | grep "$container_name" >/dev/null 2>&1; then
     docker run -d -v ${PWD}:/sources/ --security-opt=seccomp:unconfined \
         -e 'GCC_COLORS="error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01"' \
-        --name "$container_name" -it agodio/itba-so:1.0
+        --name "$container_name" -it agodio/itba-so:2.0
     docker exec "$container_name" \
         /bin/bash -c "groupadd -g $(id -g) -o user && useradd -m -u $(id -u) -g $(id -g) -o -s /bin/bash user"
 fi
